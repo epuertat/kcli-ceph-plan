60212949def7bbcac0f4a8257f33997ae5431bd8
Ernesto Puerta <37327689+epuertat@users.noreply.github.com>
2023-05-08 22:22:47 +0200

    Create bootstrap-cluster.sh

---
+++ bootstrap-cluster.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eux
+
+export PATH=/root/bin:$PATH
+
+mkdir /root/bin
+CEPHADM="/root/bin/cephadm"
+curl --silent --output $CEPHADM --location https://github.com/ceph/ceph/raw/quincy/src/cephadm/cephadm
+chmod a+x $CEPHADM
+
+mkdir -p /etc/ceph
+mon_ip=$(hostname -I)
+
+bootstrap_extra_options='--allow-fqdn-hostname --dashboard-password-noupdate'
+
+$CEPHADM bootstrap --mon-ip $mon_ip --initial-dashboard-password {{ admin_password }} ${bootstrap_extra_options}
+
+fsid=$(cat /etc/ceph/ceph.conf | grep fsid | awk '{ print $3}')
+cephadm_shell="$CEPHADM shell --fsid ${fsid} -c /etc/ceph/ceph.conf -k /etc/ceph/ceph.client.admin.keyring"
+
+{% for number in range(1, nodes) %}
+  ssh-copy-id -f -i /etc/ceph/ceph.pub  -o StrictHostKeyChecking=no root@192.168.122.$[100 + {{ number }}]
+{% endfor %}
